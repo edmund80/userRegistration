f110b3c065161e7f0b0c4d028f6666a98e4ed401
edmund80 <125154433+edmund80@users.noreply.github.com>
2024-02-25 16:15:44 -0500

    Update settings.py

--- userRegistration/userRegistration/settings.py
+++ userRegistration/userRegistration/settings.py
@@ -33,8 +33,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'genetestemail4@gmail.com'
-EMAIL_HOST_PASSWORD = 'tyzg aslr ogbr nmdq'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 # Application definition
