690db6d01e3c009af76f61669c7eaa74e3405fc2
edmund80 <125154433+edmund80@users.noreply.github.com>
2024-02-25 16:11:37 -0500

    Update settings.py

--- userRegistration/userRegistration/settings.py
+++ userRegistration/userRegistration/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres",
-        "PASSWORD": "minime03",
+        "PASSWORD": "",
         "HOST": "localhost",
         "PORT": "5432",
 
